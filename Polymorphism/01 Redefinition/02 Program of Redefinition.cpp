0332e693ebc1a9f6e57c5870ff40ea43b07d3324
Parina Sutar <sutarparina77@gmail.com>
2026-07-24 22:53:56 +0530

    Add files via upload

---
+++ Polymorphism/01 Redefinition/02 Program of Redefinition.cpp
@@ -0,0 +1,106 @@
+using namespace std;
+#include<iostream>
+#include<conio.h>
+
+class Category                                          ///Base Class
+{
+    public :
+            int Cat_ID;
+
+            Category()
+            {
+                this->Cat_ID = 100;
+
+                cout << "\n Inside Category Constructor!! " << endl;
+            }
+
+            ~Category()
+            {
+                cout << "\n Inside Category Destructor!!" << endl;
+            }
+
+            void Display(int Num)
+            {
+                cout << "\n Inside Base Class Display 1st Function " << Num;
+            }
+
+            void Display(int N1, int N2)
+            {
+                cout << "\n Inside Base Class Display 2nd Function " << N1 << N2;
+            }
+
+            void Accept()
+            {
+                cout << "\n Enter a number : " << endl;
+            }
+};
+
+class Product : public Category                         ///Derived Class
+{
+    private :
+            int P_ID;
+
+    public :
+            int Cat_ID;
+
+            Product()
+            {
+                this->P_ID = 0;
+                this->Cat_ID = 0;
+
+                cout << "\n Inside Product Constructor!! " << endl;
+            }
+
+            ~Product()
+            {
+                cout << "\n Inside Product Destructor!!" << endl;
+            }
+
+            void Display(int Num)
+            {
+                cout << "\n Inside Derived Class Display Function " << Num << endl;
+            }
+};
+
+
+int main()
+{
+    Product Obj;
+
+    cout << Obj.Cat_ID << endl;
+
+    getch();
+
+    cout << Obj.Category::Cat_ID << endl;
+
+    Obj.Accept();
+
+    getch();
+
+    Obj.Display(7);
+
+    getch();
+
+    Obj.Category::Display(5);
+
+    getch();
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
