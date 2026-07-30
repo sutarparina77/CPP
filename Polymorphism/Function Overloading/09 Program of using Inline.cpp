5c4cd0aa362fd33cae2c721fa621cd0d84f1cfe9
Parina Sutar <sutarparina77@gmail.com>
2026-07-30 22:02:21 +0530

    Add files via upload

---
+++ Polymorphism/Function Overloading/09 Program of using Inline.cpp
@@ -0,0 +1,51 @@
+using namespace std;
+#include<iostream>
+#include<conio.h>
+
+class Overloading_Criteria                                  ///Not Allowed
+{
+    public :
+            void Fun(int Num)
+            {
+                cout << "\n Inside 1st Add()!!! " << Num << endl;
+            }
+
+            inline void Fun(int No)
+            {
+                cout << "\n Inside 2nd Add()!!! " << No << endl;
+            }
+};
+
+int main()
+{
+    const Overloading_Criteria Obj1;
+
+    Overloading_Criteria Obj2;
+
+    Obj1.Fun(10);
+
+    getch();
+
+    Obj2.Fun(73);
+
+    getch();
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
